c6031b8612188ef335e3d84b52b468839fb32739
CadenGjy <cadenguo@leansoftx.com>
2019-09-08 13:07:36 +0800

    add logo

--- Source/Tailwind.Traders.Web/ClientApp/src/pages/home/components/getapp/getapp.js
+++ Source/Tailwind.Traders.Web/ClientApp/src/pages/home/components/getapp/getapp.js
@@ -6,7 +6,7 @@ import MockupSmall from "../../../../assets/images/Mockup_Small.png";
 import MockupSmall2 from "../../../../assets/images/Mockup_Small@2x.png";
 import MockupBig from "../../../../assets/images/Mockup_Big.png";
 import MockupBig2 from "../../../../assets/images/Mockup_Big@2x.png";
-
+import logo from "../../../../assets/images/logo.png";
 const Getapp = ({ t }) => {
     return (
         <section className="getapp">
@@ -18,21 +18,21 @@ const Getapp = ({ t }) => {
                         <div className="getapp-btntext">Get it now for </div>iOS
                     </a>
                     <a className="btn btn--getapp btn--getapp--right" href="../../../../assets/images/Mockup_Small.png">
-                        <div className="getapp-btntext">Get it now for </div>Android
+                        <div className="getapp-btntext" href="../../../../assets/images/Mockup_Small.png>Get it now for </div>Android
                     </a>
                 </div>
             </header>
             <picture className="getapp__body">
                 <source
-                    srcSet={`${MockupSmall} 1x, ${MockupSmall2} 2x`}
+                    srcSet={`${logo} 1x, ${logo} 2x`}
                     media="(min-width: 20em)"
                 />
                 <source
-                    srcSet={`${MockupBig} 1x, ${MockupBig2} 2x`}
+                    srcSet={`${logo} 1x, ${logo} 2x`}
                     media="(min-width: 85.375em)"
                 />
                 <img
-                    srcSet={`${MockupSmall} 1x, ${MockupSmall2} 2x`}
+                    srcSet={`${logo} 1x, ${logo} 2x`}
                     alt="Black iPhone displaying Tailwind Traders' home screen"
                 />
             </picture>
